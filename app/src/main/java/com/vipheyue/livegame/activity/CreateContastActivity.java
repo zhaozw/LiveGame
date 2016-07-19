7998e332e8558c1b1dfd83b3a16bb8cb5f073b49
何悦 <vipheyue@foxmail.com>
2016-07-19 15:45:52 +0800

    2016-07-19 15:45:49
    2016-07-19 15:45:49

--- app/src/main/java/com/vipheyue/livegame/activity/CreateContastActivity.java
+++ app/src/main/java/com/vipheyue/livegame/activity/CreateContastActivity.java
@@ -6,13 +6,35 @@ import android.support.v7.app.AppCompatActivity;
 import com.vipheyue.livegame.R;
 import com.vipheyue.livegame.bean.ContactBean;
 
+import cn.bmob.v3.listener.UpdateListener;
+
 public class CreateContastActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_contast);
-        createBean();
+//        createBean();
+        updateBean();
+    }
+
+    private void updateBean() {
+
+        ContactBean bean = new ContactBean();
+        bean.setContactTip2("兑换请联系QQ:2222222");
+        bean.setContactTip3("赠送请联系QQ:3333333");
+bean.update(this, "672481cacc", new UpdateListener() {
+    @Override
+    public void onSuccess() {
+
+    }
+
+    @Override
+    public void onFailure(int i, String s) {
+
+    }
+});
+
     }
 
     private void createBean() {
--- app/src/main/java/com/vipheyue/livegame/bean/ContactBean.java
+++ app/src/main/java/com/vipheyue/livegame/bean/ContactBean.java
@@ -8,6 +8,24 @@ import cn.bmob.v3.BmobObject;
 public class ContactBean extends BmobObject{
     String todayTip;
     String contactTip;
+    String contactTip2;
+    String contactTip3;
+
+    public String getContactTip2() {
+        return contactTip2;
+    }
+
+    public void setContactTip2(String contactTip2) {
+        this.contactTip2 = contactTip2;
+    }
+
+    public String getContactTip3() {
+        return contactTip3;
+    }
+
+    public void setContactTip3(String contactTip3) {
+        this.contactTip3 = contactTip3;
+    }
 
     public String getTodayTip() {
         return todayTip;
